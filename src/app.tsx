bb3f0003e2c19fcbf93172b08e308b5e3a24b7de
Kyle Zheng <kylezheng73@gmail.com>
2025-10-07 10:02:30 -0400

    pass config base url to router

--- src/app.tsx
+++ src/app.tsx
@@ -9,7 +9,10 @@ import { Suspense } from "solid-js";
 export default function App() {
   return (
     <>
-      <Router root={(props) => <Suspense>{props.children}</Suspense>}>
+      <Router
+        base={import.meta.env.SERVER_BASE_URL}
+        root={(props) => <Suspense>{props.children}</Suspense>}
+      >
         <FileRoutes />
       </Router>
       <footer class="text-sm flex justify-center gap-4 px-4 py-8">
